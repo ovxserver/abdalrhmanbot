9620c75649de7b1abb4340e118e278071d4b9c4a
ovxserver <45920245+ovxserver@users.noreply.github.com>
2019-02-18 22:09:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,5 +53,20 @@ message.channel.sendEmbed(embed).then(m => m.delete(17000));}
 
 
 
+client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '#spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء 
